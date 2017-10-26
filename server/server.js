7f6a716bc3f4f9b3e0edd09d9695ae36f85a7c52
Abdalla1990 <abdalla.ahmed.ksa@gmail.com>
2017-10-26 13:29:40 -0400

    connection eventsListners

--- server/server.js
+++ server/server.js
@@ -3,12 +3,24 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
+const socketIo = require('socket.io');
+const http = require('http');
+var server = http.createServer(app);
+const io = socketIo(server);
 app.use(express.static(publicPath));
 
 
+io.on('connection', (socket) => {
+    console.log('connection is initialized');
+
+    socket.on('disconnect', () => {
+        console.log('disconnected from server');
+    });
+});
 
 
 
-app.listen(port, () => {
+
+server.listen(port, () => {
     console.log('server is running on port number : ' + port);
 })
