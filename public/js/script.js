d974ec2197f0469105a21027d2dfb7b29e9bf30e
Abdalla1990 <abdalla.ahmed.ksa@gmail.com>
2017-10-30 17:00:28 -0400

    added styling and user experience

--- public/js/script.js
+++ public/js/script.js
@@ -46,10 +46,11 @@ var submitButton = jQuery('#send-message');
 submitButton.on('click', function(e) {
     e.preventDefault();
 
-    var message = jQuery('[name=message]').val();
+    var message = jQuery('[name=message]');
     //console.log('I am working', message);
-    socket.emit('createMessage', { from: 'User', text: message }, function(serverM) {
+    socket.emit('createMessage', { from: 'User', text: message.val() }, function(serverM) {
         console.log(`server says : ${serverM}`);
+        message.val('');
     });
 
 });
@@ -60,21 +61,26 @@ locationButton.on('click', function(e) {
     if (!navigator.geolocation) {
         return jQuery('#alert').append('<p>Geolocation not supported by your browser</p>');
     } else {
+        locationButton.attr('disabled', 'disabled').text('sending the location ...');
         navigator.geolocation.getCurrentPosition(function(position) {
+
             console.log(position);
             var location = {
                 latitude: position.coords.latitude,
                 longitude: position.coords.longitude
             };
+            locationButton.removeAttr('disabled').text('Send Location');
             socket.emit('createLocation', {
                 from: 'User',
                 location: location
             }, function(serverM) {
                 console.log(serverM)
+
             });
             // return jQuery('#alert').append('<p>' + position.coords.longitude + position.coords.latitude + '</p>');
         }, function() {
-            return jQuery('#alert').append('<p>unable to fetch Location</p>');
+            jQuery('#alert').append('<p>unable to fetch Location</p>');
+            locationButton.removeAttr('disabled').text('Send Location');
         });
     }
 })
